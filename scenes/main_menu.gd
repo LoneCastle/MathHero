a21fe729b24897085660e61ef4d15ef56a127fff
LoneCastle <lonecastlestudio@gmail.com>
2024-08-23 02:55:59 -0500

    Multiplication update
    Added a multiplication mode.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -10,5 +10,9 @@ func _on_subtraction_pressed():
 	get_tree().change_scene_to_file("res://scenes/Subtraction.tscn")
 
 
+func _on_multiplication_pressed():
+	get_tree().change_scene_to_file("res://scenes/Multiplication.tscn")
+
+
 func _on_quit_pressed():
 	get_tree().quit()
